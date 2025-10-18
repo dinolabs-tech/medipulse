622974c11df87b8f4a73877d9571d83681ed78d0
DinoLabs Tech Services <olaoluwadaniel1091@gmail.com>
2025-10-18 21:04:22 +0100

    refactor: Configure database connection for remote Medipulse
    The active database connection in `db_connection.php` has been updated.
    The previous local 'medipulse' database configuration was commented out.
    The 'dinolabs_medipulse' database connection, using 'dinolabs_root' credentials, has been uncommented and activated to connect to the designated remote environment.

--- database/db_connection.php
+++ database/db_connection.php
@@ -1,29 +1,29 @@
 <?php
-// $servername = "localhost";
-// $username = "dinolabs_root";
-// $password = "foxtrot2november";
-// $dbname = "dinolabs_vpaypro";
-// $conn = new mysqli($servername, $username, $password, $dbname);
-
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-
-
-
-
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "medipulse";
+$username = "dinolabs_root";
+$password = "foxtrot2november";
+$dbname = "dinolabs_medipulse";
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Create connection to MySQL
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+
+
+
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "medipulse";
+
+// // Create connection to MySQL
+// $conn = new mysqli($servername, $username, $password);
+
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 
 // // Create database if it doesn't exist - only works in offline mode
 // $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
